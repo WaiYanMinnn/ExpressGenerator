749b5aa082c0d516e94d48b4ca2b16c8ee899e57
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-06-17 15:17:36 -0700

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -10,27 +10,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req,res)=>{
-  User.register(
-    new User({username:req.body.username}),
-    req.body.password,
-    err => {
-      if(err){
-        res.statusCode=500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({err:err});
-      }else {
-        passport.authenticate('local')(req,res,()=>{
-          res.statusCode=200;
-          res.setHeader('Content-Type','application/json');
-          res.json({success:true,status:'Registration Successful'});
-        });
-      }
-    }
+router.post('/signup', async (req, res) => {
+  try {
+    const newUser = new User({ username: req.body.username });
+    if (req.body.firstname) newUser.firstname = req.body.firstname;
+    if (req.body.lastname) newUser.lastname = req.body.lastname;
+    
+    await newUser.save();
 
-  )
+    const token = authenticate.getToken({ _id: newUser._id });
 
-
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, status: 'Registration Successful', token });
+  } catch (err) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ err: err });
+  }
 });
 router.post('/login',passport.authenticate('local',{session:false}),(req,res)=>{
   const token = authenticate.getToken({_id:req.user._id});
